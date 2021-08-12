e3a1328979e3522bd084318aef00720a399c3b02
suryavamsi6 <d.suryavamsi@gmail.com>
2021-08-12 17:34:15 +0530

    feat(JavaConfig): Implement Java Config

---
+++ src/com/surya/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package com.surya.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.surya.springdemo")
+public class SportConfig {
+}
