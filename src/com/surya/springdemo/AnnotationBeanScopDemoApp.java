983e69672247d3f290c23daed2ddcf06e4fab0c6
suryavamsi6 <d.suryavamsi@gmail.com>
2021-08-12 17:14:50 +0530

    feat(Autowired - Scope): Implement Scope using Autowired

---
+++ src/com/surya/springdemo/AnnotationBeanScopDemoApp.java
@@ -0,0 +1,20 @@
+package com.surya.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopDemoApp {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach theCoach = context.getBean("tennisCoach",Coach.class);
+        Coach alphaCoach = context.getBean("tennisCoach",Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location of theCoach: " + theCoach);
+
+        System.out.println("\nMemory location of alphaCoach: " + alphaCoach);
+
+        context.close();
+    }
+}
