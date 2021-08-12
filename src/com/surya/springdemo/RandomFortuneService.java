6951ac01b72c0a0095425be118ac41ec4a84cb99
suryavamsi6 <d.suryavamsi@gmail.com>
2021-08-12 16:02:48 +0530

    feat(Autowired): Implement Autowire and Qualifier

---
+++ src/com/surya/springdemo/RandomFortuneService.java
@@ -0,0 +1,20 @@
+package com.surya.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService{
+
+    private String[] data = {"Beware of the wolf in sheep's clothing", "Diligence is the mother of good luck",
+    "The journey is the reward"};
+    private Random myRandom = new Random();
+
+    @Override
+    public String getFortune() {
+
+        int index = myRandom.nextInt(data.length);
+        return data[index];
+    }
+}
