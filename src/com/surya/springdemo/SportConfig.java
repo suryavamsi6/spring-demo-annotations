17ee65ba98bc12dae1dfefcc063d540f770d638e
suryavamsi6 <d.suryavamsi@gmail.com>
2021-08-12 17:50:22 +0530

    feat(JavaConfig - Bean): Implement @Beab

--- src/com/surya/springdemo/SportConfig.java
+++ src/com/surya/springdemo/SportConfig.java
@@ -1,9 +1,20 @@
 package com.surya.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.surya.springdemo")
+//@ComponentScan("com.surya.springdemo")
 public class SportConfig {
+
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach(sadFortuneService());
+    }
 }
