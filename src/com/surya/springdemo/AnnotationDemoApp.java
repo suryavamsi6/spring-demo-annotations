eb8580a7f0a664083b837c31ffe39bbb0d93d4df
suryavamsi6 <d.suryavamsi@gmail.com>
2021-08-12 15:09:33 +0530

    feat(Component Scan): Implement Component Scan

---
+++ src/com/surya/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,14 @@
+package com.surya.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach theCoach = context.getBean("thatSillyCoach",Coach.class);
+        System.out.println(theCoach.getDailyWorkout());
+        context.close();
+    }
+}
---
+++ src/com/surya/springdemo/Coach.java
@@ -0,0 +1,5 @@
+package com.surya.springdemo;
+
+public interface Coach {
+    public String getDailyWorkout();
+}
